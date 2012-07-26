8796234083301d97ae5de7267426be7fad27d43b
Tatsuya Aizawa <Tatsuya.Aizawa@jrits.ricoh.co.jp>
2012-07-26 09:34:04 +0900

    first commit. command: rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiramisu::Application.config.secret_token = '795a5d83d79a8285aedd2787135c65cc7f7276c122ebeef7ae65f6485f4c2861257e8c46f9c88a7b3a0964fe662f0902d0db58b35257baf36c51b352c81045b7'
