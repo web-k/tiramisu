4004573934221a7872cc6976ad74828f7fbf15eb
Toshiharu Takematsu <info@newfish.jp>
2012-09-26 20:52:37 +0900

    オフライン等でPusherの接続が切れた間にメッセージが更新されないのを防ぐために復帰時にリロード

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,13 +19,31 @@ $('form.new_message').on('ajax:success', function(){
   $(this).find("#message_content").val("");
 });
 $(function(){
-  var pusher = new Pusher('3addd561c2cd0d1a49ed'); // Replace with your app key
-  var channel = pusher.subscribe('trms-channel');
-  channel.bind('message_added', function(data) {
-    $('ul.messages').prepend('<li><span class="time"> ' + data.time + 
-      ' </span><span class="username' + 
-      ($('#my_name').text() == data.message.user_name ? ' myself' : '') + 
-      '"> (' + data.message.user_name + ') </span><span class="content"> ' +
-       data.message.content + ' </span></li>');
-  });
+  if ($('.messages').size() > 0) {
+
+    var pusher = new Pusher('3addd561c2cd0d1a49ed');
+
+    var pusher_connection_status = "initialized";
+    pusher.connection.bind('connected', function() {
+      if (pusher_connection_status == "disconnected") {
+        location.reload();
+      }
+      pusher_connection_status = "connected";
+    });
+    pusher.connection.bind('disconnected', function() {
+      pusher_connection_status = "disconnected";
+    });
+    pusher.connection.bind('unavailable', function() {
+      pusher_connection_status = "disconnected";
+    });
+
+    var channel = pusher.subscribe('trms-channel');
+    channel.bind('message_added', function(data) {
+      $('ul.messages').prepend('<li><span class="time"> ' + data.time +
+        ' </span><span class="username' +
+        ($('#my_name').text() == data.message.user_name ? ' myself' : '') +
+        '"> (' + data.message.user_name + ') </span><span class="content"> ' +
+         data.message.content + ' </span></li>');
+    });
+  }
 });
