f42bc52d6e9d8e4e866534f3852f113f1484d8b3
Toshiharu Takematsu <info@newfish.jp>
2018-03-29 10:52:15 +0900

    webpackからCoffeeScriptを廃止

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,8 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
-const coffee =  require('./loaders/coffee')
 const erb =  require('./loaders/erb')
 
 environment.loaders.append('erb', erb)
-environment.loaders.append('coffee', coffee)
 
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
